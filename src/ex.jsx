c61bc8fe19dc204cc219c27bb09cd85044c786e7
Raffaele <puglieseraffaele51@gmail.com>
2023-12-11 18:02:57 +0100

    ex5

--- src/ex.jsx
+++ src/ex.jsx
@@ -1,9 +1,11 @@
 import { Hello } from './App';
+import { Welcome } from './Welcome';
 export function Text() {
     return(
         <div>
             <Hello />
             <p>What a beautiful day!</p>
+            <Welcome name="Jhon"/>
         </div>
     )
 }
