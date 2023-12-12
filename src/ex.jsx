08ab68f2f4357fe119ae0efd39f42c3f82e97bc7
RaffaelePugliese <145784120+RaffaelePugliese@users.noreply.github.com>
2023-12-12 23:18:13 +0100

    Update ex.jsx

--- src/ex.jsx
+++ src/ex.jsx
@@ -1,7 +1,7 @@
 import { Hello } from './App';
 import { AllertButton } from './Button';
 import { InitialValue } from './Counter';
-import { Welcome } from './Welcome';
+
 export function Text() {
     return(
         <div>
