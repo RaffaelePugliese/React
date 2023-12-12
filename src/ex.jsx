4485dd31b0457361e7b5b7893c939968b983ccca
RaffaelePugliese <145784120+RaffaelePugliese@users.noreply.github.com>
2023-12-12 23:16:48 +0100

    Update ex.jsx

--- src/ex.jsx
+++ src/ex.jsx
@@ -7,7 +7,7 @@ export function Text() {
         <div>
             <Hello />
             <p>What a beautiful day!</p>
-            <Welcome name="Jhon" age={45}/>
+            
             <AllertButton />
             <Counter />
         </div>
