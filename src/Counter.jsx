19c3f99e6f4edb3f15005929fb71b01b4780c101
Raffaele <puglieseraffaele51@gmail.com>
2023-12-12 23:00:52 +0100

    ex12

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
+import { Button1,Button2,Button3, CounterDisplay } from "./Increase";
 
-export function Counter() {
+
+export function InitialValue() {
     const [Counter,setCounter]= useState(0)
-   function HandleButton(){
-    setCounter((c)=>{
-    return c + 1
-    })
-   }
-    return <div>
-    <p>i have counted to {Counter}</p>
-    <button onClick={HandleButton}>increase</button> 
-     
-    </div>
+   function IncreaseButton(){
+      setCounter((c)=>{
+      return c + 1
+      })
+    }
+    function DecreaseButton(){
+        setCounter((c)=>{
+        return c - 1
+        })
+    }
+    function ResetButton(){
+        setCounter((c)=>{
+        return 0
+        })
+    }
+ return <div>
+       <CounterDisplay Counter={Counter} />
+      <Button1 onClick={IncreaseButton} label="Increase" /> 
+      <Button2 onClick={DecreaseButton} label="Decrease" /> 
+      <Button3 onClick={ResetButton} label="Reset" /> 
+  </div>
 }
 
 // the parameter should be a function because react read all the function every time there is an event and it can be that the parameter restart
