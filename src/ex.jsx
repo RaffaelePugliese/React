cbd1d5ede515d78cfbf9792685a3c56a0b3e5cf7
RaffaelePugliese <145784120+RaffaelePugliese@users.noreply.github.com>
2023-12-12 23:13:17 +0100

    Update ex.jsx

--- src/ex.jsx
+++ src/ex.jsx
@@ -1,12 +1,12 @@
 import { Hello } from './App';
 import { AllertButton } from './Button';
-import { Welcome } from './Welcome';
+
 export function Text() {
     return(
         <div>
             <Hello />
             <p>What a beautiful day!</p>
-            <Welcome name="Jhon" age={45}/>
+          
             <AllertButton />
         </div>
     )
